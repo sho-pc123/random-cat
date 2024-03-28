d7af5a38f1191b8e90b144ee74367b3050450478
sho-pc123 <syouteni123@gmail.com>
2024-03-28 22:43:44 +0900

    修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
 import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
   const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoding] = useState(true);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetchImage().then((newImage) => {
       setImageUrl(newImage.url);
-      setLoding(false);
-    })
+      setLoading(false);
+    });
   }, []);
-  return <div>{ loading || <img src={imageUrl}/> }</div>;
-}
 
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
+};
 export default IndexPage;
 
 type Image = {
   url: string;
-}
-
-const fetchImage = async(): Promise<Image> => {
+};
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
-  console.log(images.url);
+  console.log(images);
   return images[0];
-}
+};
