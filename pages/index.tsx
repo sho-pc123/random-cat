58fc2bb9fae4d4790b28b9acea3bb355e6bdee7e
sho-pc123 <syouteni123@gmail.com>
2024-03-28 22:51:02 +0900

    修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const IndexPage: NextPage = () => {
   };
   return (
     <div>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <button onClick={handleClick}>one more cat!</button>
       <div>{loading || <img src={imageUrl} />}</div>
     </div>
   );
